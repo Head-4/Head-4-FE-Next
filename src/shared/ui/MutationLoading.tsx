85652dfcd50ec902efd106e3b95e115a353e2812
Hina0222 <hina0222@naver.com>
2025-07-19 16:24:44 +0900

    fix: mutation 로딩 상태 ux 개선

--- src/shared/ui/MutationLoading.tsx
+++ src/shared/ui/MutationLoading.tsx
@@ -3,11 +3,26 @@
 import { useIsMutating } from '@tanstack/react-query'
 import Image from 'next/image'
 import Loading from '@/assets/Loading.gif'
+import { useEffect, useState } from 'react'
 
 export default function MutationLoading() {
   const isMutating = useIsMutating()
+  const [showLoading, setShowLoading] = useState(false)
 
-  return isMutating > 0 ? (
+  useEffect(() => {
+    if (isMutating > 0) {
+      const timeoutId = setTimeout(() => {
+        setShowLoading(true)
+      }, 300)
+      return () => clearTimeout(timeoutId)
+    } else {
+      setShowLoading(false)
+    }
+  }, [isMutating])
+
+  if (!showLoading) return null
+
+  return (
     <div className="fixed inset-0 z-90 mx-auto max-w-[500px]">
       <Image
         src={Loading}
@@ -15,7 +30,8 @@ export default function MutationLoading() {
         width={160}
         height={160}
         className="absolute top-1/2 left-1/2 z-100 -translate-x-1/2 -translate-y-1/2"
+        priority
       />
     </div>
-  ) : null
+  )
 }
